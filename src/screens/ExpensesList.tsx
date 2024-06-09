b4888fe37305cc340080d8fcfcb4799614c0c24a
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-09 12:00:08 +0300

    Listing transactions

--- src/screens/ExpensesList.tsx
+++ src/screens/ExpensesList.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { apiGetTransactions } from '@api/apiClient';
+import React, { useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 const ExpensesListScreen: React.FC = () => {
-    return <Text>List</Text>;
+    const [transactions, setTransactions] = useState([{}]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        let response;
+        try {
+            response = await apiGetTransactions();
+            const data = await response.data;
+            setTransactions(data);
+        } catch (error) {
+            console.log('------------', error);
+        }
+    };
+
+    return (
+        <View>
+            {transactions.map((transaction, i) => (
+                <Text key={i}>{transaction.amount}</Text>
+            ))}
+
+            <TouchableOpacity onPress={() => fetchData()} style={{ height: 50, width: 100, backgroundColor: 'red' }}>
+                <Text>Fetch</Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 export default ExpensesListScreen;
