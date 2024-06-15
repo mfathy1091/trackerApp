934f2dc310f6208a732fde477bcb79b2d459545f
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-15 11:36:58 +0300

    Adjust root view for full screen display

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import Home from '@screens/Home';
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 
 const App: React.FC = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{ flex: 1, height: '100%', width: '100%' }}>
             <StatusBar />
-            <ScrollView contentInsetAdjustmentBehavior="automatic">
-                <Home />
-            </ScrollView>
+            {/* <ScrollView contentInsetAdjustmentBehavior="automatic"> */}
+            <Home />
+            {/* </ScrollView> */}
         </SafeAreaView>
     );
 };
