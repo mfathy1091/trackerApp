442f92859ec2d463985d870f4f3f62e90848e381
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-15 19:19:19 +0300

    Adding account SelectField to the AddRecord screen

--- src/navigators/home/screens/AddRecordScreen.tsx
+++ src/navigators/home/screens/AddRecordScreen.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { StyleSheet, View } from 'react-native';
+import SingleSelect from '../components/SingleSelect';
 
 const AddRecordScreen: React.FC = () => {
-    return <></>;
+    const options = [
+        { name: 'Food', option: 'food' },
+        { name: 'Transportation', option: 'transportation' },
+    ];
+
+    const { control } = useForm<FormData>({
+        mode: 'all',
+        defaultValues: {
+            category: '',
+        },
+    });
+
+    return (
+        <View>
+            <Controller
+                control={control}
+                name="category"
+                render={({ field: { onChange, value } }) => (
+                    <SingleSelect
+                        title={'Category'}
+                        selectedKey={value}
+                        options={options}
+                        fields={{ key: 'option', text: 'name' }}
+                        onChangeSelection={onChange}
+                    />
+                )}
+            />
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({});
 
 export default AddRecordScreen;
