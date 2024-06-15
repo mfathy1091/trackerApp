9ace9aeff1416e40cf4e3ce56c4bb76eab83cb4c
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-15 14:41:18 +0300

    Creating RecordsList screen

---
+++ src/navigators/home/screens/RecordsList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ExpensesList from '../components/ExpensesList';
+
+const RecordsListScreen: React.FC = () => {
+    return <ExpensesList />;
+};
+
+export default RecordsListScreen;
