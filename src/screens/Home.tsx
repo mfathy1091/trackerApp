8b8480a581bccb05222ce488bed594958e312e65
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-15 12:31:19 +0300

    Enchanced Home screen layout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,33 @@
 import AccountsList from '@components/AccountsList';
 import GearIcon from '@icons/gear.svg';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import theme from 'theme';
 
 const HomeScreen: React.FC = () => {
     return (
-        <View>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>List of accounts</Text>
-                <GearIcon />
+        <View style={styles.container}>
+            <View style={styles.accountsContainer}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>List of accounts</Text>
+                    <GearIcon />
+                </View>
+                <AccountsList />
             </View>
-            <AccountsList />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: theme.color.lightGrey,
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+    accountsContainer: {
+        backgroundColor: 'white',
+    },
     titleContainer: {
         height: 50,
         flexDirection: 'row',
