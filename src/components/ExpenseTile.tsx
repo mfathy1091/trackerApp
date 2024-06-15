b1e7224d787edf3fbd0594a3ce1e273810383873
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-15 10:55:52 +0300

    Refactored ExpenseTile

--- src/components/ExpenseTile.tsx
+++ src/components/ExpenseTile.tsx
@@ -1,30 +1,72 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FastImage from 'react-native-fast-image';
 import theme from '../theme';
 
 const ExpenseTile = ({ transaction }) => {
-    const color = transaction.type === 'credit' ? theme.color.moderateBlue : theme.color.earthyRed;
+    let color;
+    let amountText = `EGP ${transaction.amount}`;
+    switch (transaction?.type) {
+        case 'credit':
+            color = theme.color.green;
+            break;
+        case 'debit':
+            color = theme.color.red;
+            amountText = `-${amountText}`;
+            break;
+    }
     return (
-        <View style={styles.container}>
-            <View style={{ flexDirection: 'row' }}>
-                <Text>Icon</Text>
-                <Text style={{ width: 66 }}>{transaction?.category?.name}</Text>
-                <Text style={{ width: 66 }}>{transaction?.account?.name}</Text>
+        <View style={{ alignItems: 'center' }}>
+            <View style={styles.container}>
+                <View style={styles.left}>
+                    <FastImage
+                        style={styles.categoryIcon}
+                        source={{
+                            uri: transaction?.category?.icon,
+                            priority: FastImage.priority.normal,
+                        }}
+                        resizeMode={FastImage.resizeMode.contain}
+                    />
+                    <View style={{ marginLeft: 15 }}>
+                        <Text style={styles.categoryName}>{transaction?.category?.name}</Text>
+                        <Text style={styles.accountName}>{transaction?.account?.name}</Text>
+                    </View>
+                </View>
+                <View style={styles.right}>
+                    <Text style={{ color: color, ...theme.text.inputText }}>{amountText}</Text>
+                    <Text style={styles.date}>{transaction?.date}</Text>
+                </View>
             </View>
-            <Text style={{ color: color, ...theme.text.inputText }}>{transaction?.amount}</Text>
+            <View style={styles.separator}></View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        height: 51,
+        height: 65,
         width: '100%',
-        borderWidth: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         padding: 16,
+    },
+    left: { flexDirection: 'row', alignItems: 'center' },
+    right: { alignItems: 'flex-end' },
+    separator: { height: 1, width: '95%', backgroundColor: theme.color.strokeButton },
+    categoryName: {
+        color: theme.color.black,
+        ...theme.text.inputText,
+    },
+    categoryIcon: { height: 37, width: 37 },
+    accountName: {
+        color: theme.color.grey,
+        ...theme.text.inputText,
+    },
+    amount: {},
+    date: {
+        color: theme.color.grey,
+        ...theme.text.inputText,
     },
 });
 
