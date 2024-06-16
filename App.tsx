d1b99c5c150e05b8a017b42e0018f9853b0f4fa8
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-16 10:03:55 +0300

    Configuring app to use react-query

--- App.tsx
+++ App.tsx
@@ -2,14 +2,19 @@ import 'react-native-gesture-handler';
 
 import AppNavigator from '@navigators/appNavigator';
 import { NavigationContainer, createNavigationContainerRef } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
 const App: React.FC = () => {
+    const queryClient = new QueryClient();
     const navigationRef = createNavigationContainerRef();
+
     return (
-        <NavigationContainer ref={navigationRef}>
-            <AppNavigator />
-        </NavigationContainer>
+        <QueryClientProvider client={queryClient}>
+            <NavigationContainer ref={navigationRef}>
+                <AppNavigator />
+            </NavigationContainer>
+        </QueryClientProvider>
 
         // <SafeAreaView style={{ flex: 1, height: '100%', width: '100%' }}>
         //     <StatusBar />
